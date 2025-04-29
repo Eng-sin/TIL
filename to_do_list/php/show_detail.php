d2c8a0d1ea7faa774d3230cfcd595dea4fa872ba
Eng-sin <111264731+Eng-sin@users.noreply.github.com>
2025-04-29 22:41:49 +0900

    メモをマークダウン記法に対応

--- to_do_list/php/show_detail.php
+++ to_do_list/php/show_detail.php
@@ -1,6 +1,10 @@
 <?php
+require('vendor/autoload.php');
 require_once('config.php');
 require_once('common_utils.php');
+
+$parsedown = new Parsedown();
+
 function showDetail()
 {
   global $deadlineDate, $content, $taskStatus, $priority, $row, $columns;
@@ -58,6 +62,8 @@ showDetail();
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/detail.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://unpkg.com/easymde/dist/easymde.min.css">
+  <script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
 </head>
 
 <body>
@@ -90,7 +96,7 @@ showDetail();
     <div class="memo__items">
       <?php foreach ($columns as $memo): ?>
       <div class="memo__item">
-        <p class="memo__item__explain"><?php echo htmlspecialchars($memo['memo'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="memo__item__explain"><?php echo $parsedown->text($memo['memo']); ?></p>
         <form class="memo__item__delete" action="delete_memo.php" method="post">
           <input type="hidden" value="<?php echo htmlspecialchars($memo['memo_id'], ENT_QUOTES, 'UTF-8') ?>"
             name="memo_id">
@@ -110,7 +116,12 @@ showDetail();
   </div>
   <?php endif; ?>
   <form class="regist__new__memo" action="regist_new_memo.php" method="post">
-    <textarea class="new_memo_text" name="new_memo_text"></textarea>
+    <textarea class="new_memo_text" name="new_memo_text" id="new_memo_text"></textarea>
+    <script>
+    const easyMDE = new EasyMDE({
+      element: document.getElementById("new_memo_text"),
+    });
+    </script>
     <input type="hidden" name="task_id" value="<?= htmlspecialchars($_GET["task_id"], ENT_QUOTES, "UTF-8") ?>">
     <button name="regist_new_memo">メモを追加</button>
   </form>
