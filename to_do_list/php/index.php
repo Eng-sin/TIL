4c4acab93e543ffbdd38ed1eb02d9fbc8adfa842
Eng-sin <111264731+Eng-sin@users.noreply.github.com>
2025-05-14 08:37:53 +0900

    担当者をタスク一覧画面に追加、担当者の簡易オートコンプリート機能実装

--- to_do_list/php/index.php
+++ to_do_list/php/index.php
@@ -1,5 +1,5 @@
 <?php
-global $result, $taskCount;
+global $result, $taskCount, $managerTaskCount;
 require_once('config.php');
 require_once('common_utils.php');
 
@@ -12,25 +12,25 @@ $completeFlag = array_key_exists("hidden_complete_flg", $_POST);
 $otherPeopleFlag = array_key_exists("hidden_other_people_task_flg", $_POST);
 
 if ($completeFlag && $otherPeopleFlag) {
-  $sql = "SELECT * FROM t_todo where task_status in ('未着手','進行中') and user_id = ?
+  $sql = "SELECT * FROM t_todo T left outer join users U on T.manager_id = U.id where task_status in ('未着手','進行中') and T.user_id = ?
           ORDER BY 
             FIELD(task_status,'未着手','進行中'),
             deadline_date,
             FIELD(priority,'高','中','低')";
 } elseif ($completeFlag && !$otherPeopleFlag) {
-  $sql = "SELECT * FROM t_todo where task_status in ('未着手','進行中') and (user_id = ? or publication_range = '公開')
+  $sql = "SELECT * FROM t_todo T left outer join users U on T.manager_id = U.id where task_status in ('未着手','進行中') and (user_id = ? or publication_range = '公開')
           ORDER BY 
             FIELD(task_status,'未着手','進行中','完了'),
             deadline_date,
             FIELD(priority,'高','中','低')";
 } elseif (!$completeFlag && $otherPeopleFlag) {
-  $sql = "SELECT * FROM t_todo where user_id = ?
+  $sql = "SELECT * FROM t_todo T left outer join users U on T.manager_id = U.id where user_id = ?
           ORDER BY 
             FIELD(task_status,'未着手','進行中','完了'),
             deadline_date,
             FIELD(priority,'高','中','低')";
 } else {
-  $sql = "SELECT * FROM t_todo where user_id = ? or publication_range = '公開'
+  $sql = "SELECT * FROM t_todo T left outer join users U on T.manager_id = U.id where user_id = ? or publication_range = '公開'
           ORDER BY 
             FIELD(task_status,'未着手','進行中'),
             deadline_date,
@@ -71,6 +71,27 @@ if (!$taskCounts) {
 $taskCount = [];
 while ($row = $taskCounts->fetch_assoc()) {
   $taskCount[$row["task_status"]] = $row["count"];
+}
+
+if ($completeFlag && $otherPeopleFlag) {
+  $sql = "SELECT count(*) as count FROM t_todo where task_status in ('未着手','進行中') and user_id = ? and manager_id = ?";
+} elseif ($completeFlag && !$otherPeopleFlag) {
+  $sql = "SELECT count(*) as count FROM t_todo where task_status in ('未着手','進行中') and (user_id = ? or publication_range = '公開') and manager_id = ?";
+} elseif (!$completeFlag && $otherPeopleFlag) {
+  $sql = "SELECT count(*) as count FROM t_todo where user_id = ? and manager_id = ?";
+} else {
+  $sql = "SELECT count(*) as count FROM t_todo where (user_id = ? or publication_range = '公開') and manager_id = ?";
+}
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("ss", $_SESSION["userid"], $_SESSION["userid"]);
+$stmt->execute();
+$managerTaskCounts = $stmt->get_result();
+if (!$managerTaskCounts) {
+  die("クエリの実行に失敗しました: " . $mysqli->error);
+}
+$managerTaskCount = 0;
+while ($row = $managerTaskCounts->fetch_assoc()) {
+  $managerTaskCount = $row["count"];
 }
 
 $mysqli->close();
@@ -116,7 +137,8 @@ $mysqli->close();
   </header>
   <p class="main"><?php echo "未着手:" . ($taskCount["未着手"] ?? 0) . "件";
                   echo "　進行中:" . ($taskCount["進行中"] ?? 0) . "件";
-                  echo "　完了:" . ($taskCount["完了"] ?? 0) . "件"; ?></p>
+                  echo "　完了:" . ($taskCount["完了"] ?? 0) . "件";
+                  echo "　自分担当のタスク:" . ($managerTaskCount ?? 0) . "件"; ?></p>
   <div class="main">
     <table class="main__content">
       <tr class="main__content__columns">
@@ -125,6 +147,7 @@ $mysqli->close();
         <th class="main__content__columns__add__border">内容</th>
         <th class="main__content__columns__add__border">進捗状況</th>
         <th class="main__content__columns__add__border">優先度</th>
+        <th class="main__content__columns__add__border">担当者</th>
         <th></th>
         <th></th>
       </tr>
@@ -149,6 +172,8 @@ $mysqli->close();
             <?php echo htmlspecialchars($column['task_status'], ENT_QUOTES, 'UTF-8')  ?></td>
           <td class="main__content__row__add__border<?php echo $statusClass ?>">
             <?php echo htmlspecialchars($column['priority'], ENT_QUOTES, 'UTF-8')  ?></td>
+          <td class="main__content__row__add__border<?php echo $statusClass ?>">
+            <?php echo htmlspecialchars($column['name'] ?? '-', ENT_QUOTES, 'UTF-8')  ?></td>
           <?php if ($column['user_id'] == $_SESSION['userid']): ?>
             <form class="main__content__row" action="edit_task.php" method="post">
               <td>
