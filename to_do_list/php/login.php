66af302bacf4b20b8bca8e53533e24abfc4b54fd
Eng-sin <111264731+Eng-sin@users.noreply.github.com>
2025-05-18 08:33:19 +0900

    ログイン画面をtailwind cssで実装

--- to_do_list/php/login.php
+++ to_do_list/php/login.php
@@ -37,7 +37,7 @@ function login()
 
     $passwordDb = $result["password"];
     if (!password_verify($password, $passwordDb)) {
-      $errorMessage = "ユーザー名またはパスワードが違います。ぱ";
+      $errorMessage = "ユーザー名またはパスワードが違います。";
       return $errorMessage;
     }
 
@@ -69,30 +69,37 @@ if (array_key_exists("login_button", $_POST)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/login.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-  <div class="wrapper">
-    <?php if (!empty($_GET["isComplete"])) : ?>
-    <div class="complete__regist__new__member">
-      <p>新規会員登録が成功しました。</p>
-    </div>
-    <?php endif; ?>
-    <h1 class="login__title">ログイン画面</h1>
-    <form action="login.php" method="post" class="login__form">
-      <?php if (!empty($errorMessage)) : ?>
-      <p class="error__message"><?= htmlspecialchars($errorMessage, ENT_QUOTES, "UTF-8") ?></p>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-96">
+      <?php if (!empty($_GET["isComplete"])) : ?>
+        <p class="bg-green-100 text-green-700 p-4 rounded-md mb-4 text-sm">新規会員登録が成功しました。</p>
       <?php endif; ?>
-      ユーザー名
-      <input type="text" name="username"
-        value="<?= htmlspecialchars($_POST['username'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-      パスワード
-      <input type="password" name="password">
-      <button name="login_button">ログイン</button>
-    </form>
-    <a href="regist_new_member.php">新規会員登録はこちら</a>
+      <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">ログイン画面</h1>
+      <form action="login.php" method="post" class="mb-4">
+        <?php if (!empty($errorMessage)) : ?>
+          <p class="bg-red-100 text-red-700 p-4 rounded-md mb-4 text-sm">
+            <?= htmlspecialchars($errorMessage, ENT_QUOTES, "UTF-8") ?></p>
+        <?php endif; ?>
+        <div class="mb-4">
+          <label for="username" class="block text-sm font-medium text-gray-700">ユーザー名</label>
+          <input class="mt-1 w-full px-4 py-2 border border-red-500 rounded-md shadow-sm" type="text" name="username"
+            value="<?= htmlspecialchars($_POST['username'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block text-sm font-medium text-gray-700">パスワード</label>
+          <input class="mt-1 w-full px-4 py-2 border border-red-500 rounded-md shadow-sm" type="password"
+            name="password">
+        </div>
+
+        <button name="login_button"
+          class="w-full bg-sky-500 text-white px-4 py-2 rounded-md shadow hover:bg-sky-600">ログイン</button>
+      </form>
+      <a class="block w-full text-center text-gray-800 hover:text-gray-500" href="regist_new_member.php">新規会員登録はこちら</a>
+    </div>
   </div>
 
 </body>
--- to_do_list/php/regist_new_member.php
+++ to_do_list/php/regist_new_member.php
@@ -67,25 +67,33 @@ if (array_key_exists("regist_new_member_button", $_POST)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/regist_new_member.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-  <div class="wrapper">
-    <h1 class="regist__new__member__title">新規会員登録画面</h1>
-    <form action="regist_new_member.php" method="post" class="regist__new__member__form">
-      <?php if (!empty($errorMessage)) : ?>
-      <p class="error__message"><?= htmlspecialchars($errorMessage, ENT_QUOTES, "UTF-8") ?></p>
-      <?php endif; ?>
-      ユーザー名
-      <input type="text" name="username"
-        value="<?= htmlspecialchars($_POST['username'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
-      パスワード
-      <input type="password" name="password">
-      <button name="regist_new_member_button">登録する</button>
-    </form>
-    <a href="login.php">ログイン画面に戻る</a>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-96">
+      <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">新規会員登録画面</h1>
+      <form action="regist_new_member.php" method="post" class="mb-4">
+        <?php if (!empty($errorMessage)) : ?>
+          <p class="bg-red-100 text-red-700 p-4 rounded-md mb-4 text-sm">
+            <?= htmlspecialchars($errorMessage, ENT_QUOTES, "UTF-8") ?></p>
+        <?php endif; ?>
+        <div class="mb-4">
+          <label for="username" class="block text-sm font-medium text-gray-700">ユーザー名</label>
+          <input class="mt-1 w-full px-4 py-2 border border-red-500 rounded-md shadow-sm" type="text" name="username"
+            value="<?= htmlspecialchars($_POST['username'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block text-sm font-medium text-gray-700">パスワード</label>
+          <input class="mt-1 w-full px-4 py-2 border border-red-500 rounded-md shadow-sm" type="password"
+            name="password">
+        </div>
+        <button name="regist_new_member_button"
+          class="w-full px-4 py-2 text-white bg-sky-500 rounded-md shadow hover:bg-sky-600">登録する</button>
+      </form>
+      <a class="block w-full text-center text-gray-800 hover:text-gray-500" href="login.php">ログイン画面に戻る</a>
+    </div>
   </div>
 
 </body>
